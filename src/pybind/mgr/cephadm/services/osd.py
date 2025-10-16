439418c6edddf56744aeb0823e77622cb6448625
Vivek Maurya <vivek.maurya2@ibm.com>
2025-10-16 08:38:45 -0500

    test changes3

--- src/pybind/mgr/cephadm/services/osd.py
+++ src/pybind/mgr/cephadm/services/osd.py
@@ -316,7 +316,7 @@ class OSDService(CephService):
 
                 # get preview data from ceph-volume
                 for cmd in cmds:
-                    with self.mgr.async_timeout_handler(host, f'cephadm ceph-volume --log-level {self.mgr.ceph_volume_log_level}, -- {cmd}'):
+                    with self.mgr.async_timeout_handler(host, f'cephadm ceph-volume --log-level {self.mgr.ceph_volume_log_level} -- {cmd}'):
                         out, err, code = self.mgr.wait_async(self._run_ceph_volume_command(host, cmd))
                     if out:
                         try:
@@ -384,8 +384,9 @@ class OSDService(CephService):
         split_cmd = cmd.split(' ')
         _cmd = ['--config-json', '-', '--']
         _cmd.extend(split_cmd)
+        _cmd = ['--log-level', self.mgr.ceph_volume_log_level] + _cmd
         out, err, code = await CephadmServe(self.mgr)._run_cephadm(
-            host, 'osd', f'ceph-volume --log-level {self.mgr.ceph_volume_log_level}',
+            host, 'osd', 'ceph-volume',
             _cmd,
             env_vars=env_vars,
             stdin=j,
